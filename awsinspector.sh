b0d4c00be55508d3948dc108a74f39867d030341
Seetal Bhambra <s.bhambra@resilientplc.com>
2022-03-07 11:56:32 +0000

    created installation method for falcon + error file

--- awsinspector.sh
+++ awsinspector.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Set sudo super user
+sudo su -
+
 # Installing AWS Inspector on Linux based EC2 instances
 wget https://inspector-agent.amazonaws.com/linux/latest/install
-sudo bash install
+bash install
 
 # File Verification
 FILE=("libcurl4" "libcurl3" "libgcc1" "libc6" "libstdc++6" "libssl1.0.1" "libssl1.0.2" "libssl1.1" "libpcap0.8")
@@ -11,3 +14,19 @@ if [ -f "$FILE" ]; then
 else 
     echo "$FILE does not exist."
 fi 
+
+
+INSTALLER= $(aws s3 cp s3://resilient-dev-falcon-installer/ /home/ec2-user )
+if [ -f "$INSTALLER"]; then 
+    cd /home/ec2-user
+    chmod u+x install
+    ./install
+else 
+    "$INSTALLER" > output.txt
+    mkdir home/ec2-user/falcon
+    mv output.txt home/ec2-user/falcon
+fi
+
+
+
+
